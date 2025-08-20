8d802ffb29a682f23f7e2c8dcbc8ccf950a561dd
Lucas Paulo Venceslau Dantas <luvences@c3r7p2.42sp.org.br>
2025-08-20 16:47:49 -0300

    colando os headers que faltavam

--- ft_substr.c
+++ ft_substr.c
@@ -1,33 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: luvences <luvences@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/20 16:44:59 by luvences          #+#    #+#             */
-/*   Updated: 2025/08/20 16:45:02 by luvences         ###   ########.fr       */
+/*   Created: 2025/08/20 16:43:38 by luvences          #+#    #+#             */
+/*   Updated: 2025/08/20 16:46:35 by luvences         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	size_little;
-
-	i = 0;
-	size_little = ft_strlen(little);
-	if (*little == '\0')
-		return ((char *)big);
-	while (i < len && big[i] != '\0')
-	{
-		if (i + size_little > len)
-			return (NULL);
-		if (ft_strncmp(big + i, little, size_little) == 0)
-			return ((char *)(big + i));
-		i++;
-	}
-	return (NULL);
 }
